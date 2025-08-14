ff0bac7b44366a67e016c2406c15757a5e007fb5
jorawaraksk <jorawarzu@proton.me>
2025-08-14 21:50:28 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import threading
+from main import main
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is Running!", 200
+
+def run_flask():
+    app.run(host='0.0.0.0', port=8000)
+
+if __name__ == "__main__":
+    threading.Thread(target=run_flask).start()
+    main()
