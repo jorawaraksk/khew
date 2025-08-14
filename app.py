659c16e15151d423fb348d3b8f54e8b9087f0443
jorawaraksk <jorawarzu@proton.me>
2025-08-14 22:50:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 import threading
 from main import main
@@ -8,8 +9,11 @@ app = Flask(__name__)
 def home():
     return "Bot is Running!", 200
 
+import os
+
 def run_flask():
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get("PORT", 8000))  # use Render's assigned port
+    app.run(host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
     threading.Thread(target=run_flask).start()
