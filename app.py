785ece495c02505e2cb06c46b15cf2355fdae09d
jorawaraksk <jorawarzu@proton.me>
2025-08-14 22:51:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ app = Flask(__name__)
 def home():
     return "Bot is Running!", 200
 
-import os
-
 def run_flask():
     port = int(os.environ.get("PORT", 8000))  # use Render's assigned port
     app.run(host="0.0.0.0", port=port)
